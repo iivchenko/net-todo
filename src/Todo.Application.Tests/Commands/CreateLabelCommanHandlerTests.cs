9da5c975e774369a051d83b42cd5c4fad656c2eb
iivchenko <iivchenko@live.com>
2020-10-10 11:42:37 +0200

    Implement Create Label Command

---
+++ src/Todo.Application.Tests/Commands/CreateLabelCommanHandlerTests.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Todo.Application.Commands;
+using Todo.Application.Domain.Common;
+using Todo.Application.Domain.LableAggregate;
+
+namespace Todo.Application.Tests.Commands
+{
+    [TestFixture]
+    public sealed class CreateLabelCommanHandlerTests
+    {
+        private CreateLabelCommanHandler _handler;
+
+        private Mock<IRepository<Label, Guid>> _labelRepository;
+        private Mock<IUnitOfWork> _unitOfWork;
+
+        [SetUp]
+        public void Setup()
+        {
+            _labelRepository = new Mock<IRepository<Label, Guid>>();
+
+            _unitOfWork = new Mock<IUnitOfWork>();
+
+            _handler = new CreateLabelCommanHandler(_labelRepository.Object, _unitOfWork.Object);
+        }
+
+        [Test]
+        public async Task Handle_Success()
+        {
+            // Arrange
+            var command = new CreateLabelCommand
+            {
+                Name = "test-name",
+                Description = "test-description",
+                Color = new CreateLabelCommanColor()
+            };
+
+            var expectedLabelId = Guid.NewGuid(); 
+
+            _labelRepository
+                .Setup(x => x.CreateAsync(It.IsAny<Label>()))
+                .ReturnsAsync(expectedLabelId);
+
+            // Act
+            var response = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            _labelRepository.Verify(x => x.CreateAsync(It.IsAny<Label>()), Times.Once);
+            _unitOfWork.Verify(x => x.CommitAsync(), Times.Once);
+
+            Assert.That(response.Id, Is.EqualTo(expectedLabelId));
+        }
+    }
+}
---
+++ src/Todo.Application/Commands/CreateLabel.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Todo.Application.Domain.Common;
+using Todo.Application.Domain.LableAggregate;
+
+namespace Todo.Application.Commands
+{
+    public sealed class CreateLabelCommand : IRequest<CreateLabelCommandResponse>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public CreateLabelCommanColor Color { get; set; }
+    }
+
+    public sealed class CreateLabelCommanColor
+    {
+        public byte Red { get; set; }
+
+        public byte Green { get; set; }
+
+        public byte Blue { get; set; }
+
+        public byte Alpha { get; set; }
+    }
+
+    public sealed class CreateLabelCommanHandler : IRequestHandler<CreateLabelCommand, CreateLabelCommandResponse>
+    {
+        private readonly IRepository<Label, Guid> _labelRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateLabelCommanHandler(
+            IRepository<Label, Guid> labelRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _labelRepository = labelRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<CreateLabelCommandResponse> Handle(CreateLabelCommand request, CancellationToken cancellationToken)
+        {
+            var color = new Color(request.Color.Red, request.Color.Green, request.Color.Blue, request.Color.Alpha);
+            var label = new Label(Guid.NewGuid(), request.Name, request.Description, color);
+
+            var id = await _labelRepository.CreateAsync(label);
+
+            await _unitOfWork.CommitAsync();
+
+            return new CreateLabelCommandResponse
+            {
+                Id = id
+            };
+        }
+    }
+
+    public sealed class CreateLabelCommandResponse
+    {
+        public Guid Id { get; set; }
+    }
+}

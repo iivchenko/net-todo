73b11884a8e208304dc5ab8076697639e135b0e8
iivchenko <iivchenko@live.com>
2020-10-10 14:49:44 +0200

    Add missing using

--- src/Todo.Infrastructure.Persistence.MsSql/TodoItemAggregate/LableToItemMapping.cs
+++ src/Todo.Infrastructure.Persistence.MsSql/TodoItemAggregate/LableToItemMapping.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using Todo.Application.Domain.LableAggregate;
 using Todo.Application.Domain.TodoItemAggregate;
 

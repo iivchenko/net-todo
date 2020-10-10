8b5e91c38ebb86dddbe76d98d33c3584f6164b23
iivchenko <iivchenko@live.com>
2020-10-10 14:23:46 +0200

    Implement Update Label Command

---
+++ src/Todo.Application.Tests/Commands/UpdateLabelCommanHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Todo.Application.Commands;
+using Todo.Application.Domain.Common;
+using Todo.Application.Domain.LableAggregate;
+
+namespace Todo.Application.Tests.Commands
+{
+    [TestFixture]
+    public sealed class UpdateLabelCommanHandlerTests
+    {
+        private UpdateLabelCommanHandler _handler;
+
+        private Mock<IRepository<Label, Guid>> _labelRepository;
+        private Mock<IUnitOfWork> _unitOfWork;
+
+        [SetUp]
+        public void Setup()
+        {
+            _labelRepository = new Mock<IRepository<Label, Guid>>();
+
+            _unitOfWork = new Mock<IUnitOfWork>();
+
+            _handler = new UpdateLabelCommanHandler(_labelRepository.Object, _unitOfWork.Object);
+        }
+
+        [Test]
+        public async Task Handle_Success()
+        {
+            // Arrange
+            var labelId = Guid.NewGuid();
+
+            var command = new UpdateLabelCommand
+            {
+                Id = labelId,
+                Name = "new-name",
+                Description = "new-description",
+                Color = new UpdateLabelCommanColor
+                {
+                    Red = 10,
+                    Green = 11,
+                    Blue = 12,
+                    Alpha = 13
+                }
+            };
+
+            var label = new Label(labelId, "test-name", "test-description", new Color(1, 2, 3, 4));
+
+            _labelRepository
+                .Setup(x => x.FindByIdAsync(labelId))
+                .ReturnsAsync(label);
+
+            // Act
+            await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            _labelRepository.Verify(x => x.UpdateAsync(label), Times.Once);
+            _unitOfWork.Verify(x => x.CommitAsync(), Times.Once);
+
+            Assert.That(label.Name, Is.EqualTo("new-name"));
+            Assert.That(label.Description, Is.EqualTo("new-description"));
+            Assert.That(label.Color, Is.EqualTo(new Color(10, 11, 12, 13)));
+        }
+    }
+}
---
+++ src/Todo.Application/Commands/UpdateLabel.cs
@@ -0,0 +1,71 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Todo.Application.Domain.Common;
+using Todo.Application.Domain.LableAggregate;
+
+namespace Todo.Application.Commands
+{
+    public sealed class UpdateLabelCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public UpdateLabelCommanColor Color { get; set; }
+    }
+
+    public sealed class UpdateLabelCommanColor
+    {
+        public byte Red { get; set; }
+
+        public byte Green { get; set; }
+
+        public byte Blue { get; set; }
+
+        public byte Alpha { get; set; }
+    }
+
+    public sealed class UpdateLabelCommanHandler : IRequestHandler<UpdateLabelCommand, Unit>
+    {
+        private readonly IRepository<Label, Guid> _labelRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateLabelCommanHandler(
+            IRepository<Label, Guid> labelRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _labelRepository = labelRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Unit> Handle(UpdateLabelCommand request, CancellationToken cancellationToken)
+        {
+            var label = await _labelRepository.FindByIdAsync(request.Id);
+            var color = new Color(request.Color.Red, request.Color.Green, request.Color.Blue, request.Color.Alpha);
+
+            if (label.Name != request.Name)
+            {
+                label.UpdateName(request.Name);
+            }
+
+            if (label.Description != request.Description)
+            {
+                label.UpdateDescription(request.Description);
+            }
+
+            if (label.Color != color)
+            {
+                label.UpdateColor(color);
+            }
+
+            await _labelRepository.UpdateAsync(label);
+            await _unitOfWork.CommitAsync();
+
+            return Unit.Value;
+        }
+    }
+}
